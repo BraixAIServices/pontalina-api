b3c06035d648c9a90498d281786b79018e7deb80
TheDataResearch <drocha21@gmail.com>
2018-10-02 00:06:24 -0300

    Conexão com facebook

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 const watson = require('watson-developer-cloud/assistant/v1');
 
 const chatbot = new watson({
@@ -13,14 +15,32 @@ const workspace_id = '91873e3d-99c4-4d24-8a89-26b38b5f0322';
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
+var verify = require('./security');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({verify: verify}));
 app.listen((process.env.PORT || 5000));
 
 //Início da conversação
-chatbot.message({workspace_id}, trataResposta);
+//chatbot.message({workspace_id}, trataResposta);
+
+module.exports = function(app) {
+    var Facebook = require('./bot-facebook');
+    Facebook.controller.middleware.receive.use(middleware.receive);
+    Facebook.controller.createWebhookEndpoints(app, Facebook.bot);
+    console.log('Facebook bot is live');
+ // Customize your Watson Middleware object's before and after callbacks.
+    
+    middleware.before = function(message, assistantPayload, callback) {
+        callback(null, assistantPayload);
+    };
+
+    middleware.after = function(message, assistantResponse, callback) {
+        callback(null, assistantResponse);
+    };
+};
+
 
 function trataResposta(err, resposta){
 
@@ -43,7 +63,7 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook/facebook", function (req, res) {
-    if (req.query["hub.verify_token"] === "chatbotpontalina2019") {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
       console.log("Verified webhook");
       res.status(200).send(req.query["hub.challenge"]);
     } else {
