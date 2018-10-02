3648bc6be7044ee590cbea813e892d9882f7e998
TheDataResearch <drocha21@gmail.com>
2018-10-02 00:26:17 -0300

    Versão watson toolkit

--- app.js
+++ app.js
@@ -1,73 +1,54 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 require('dotenv').load();
 
-const watson = require('watson-developer-cloud/assistant/v1');
-
-const chatbot = new watson({
-
-    username: '015c1b44-dbd6-4121-9fcf-8f4280d557bf',
-    password: 'zDLtpR883k27',
-    version: '2018-09-30',
-
+var middleware = require('botkit-middleware-watson')({
+  username: process.env.ASSISTANT_USERNAME,
+  password: process.env.ASSISTANT_PASSWORD,
+  workspace_id: process.env.WORKSPACE_ID,
+  url: process.env.ASSISTANT_URL || 'https://gateway.watsonplatform.net/assistant/api',
+  version: '2018-07-10'
 });
 
-const workspace_id = '91873e3d-99c4-4d24-8a89-26b38b5f0322';
-
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
-var verify = require('./security');
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json({verify: verify}));
-app.listen((process.env.PORT || 5000));
-
-//Início da conversação
-//chatbot.message({workspace_id}, trataResposta);
-
 module.exports = function(app) {
+  if (process.env.USE_SLACK) {
+    var Slack = require('./bot-slack');
+    Slack.controller.middleware.receive.use(middleware.receive);
+    Slack.bot.startRTM();
+    console.log('Slack bot is live');
+  }
+  if (process.env.USE_FACEBOOK) {
     var Facebook = require('./bot-facebook');
     Facebook.controller.middleware.receive.use(middleware.receive);
     Facebook.controller.createWebhookEndpoints(app, Facebook.bot);
     console.log('Facebook bot is live');
- // Customize your Watson Middleware object's before and after callbacks.
-    
-    middleware.before = function(message, assistantPayload, callback) {
-        callback(null, assistantPayload);
-    };
+  }
+  if (process.env.USE_TWILIO) {
+    var Twilio = require('./bot-twilio');
+    Twilio.controller.middleware.receive.use(middleware.receive);
+    Twilio.controller.createWebhookEndpoints(app, Twilio.bot);
+    console.log('Twilio bot is live');
+  }
+  // Customize your Watson Middleware object's before and after callbacks.
+  middleware.before = function(message, assistantPayload, callback) {
+    callback(null, assistantPayload);
+  };
 
-    middleware.after = function(message, assistantResponse, callback) {
-        callback(null, assistantResponse);
-    };
+  middleware.after = function(message, assistantResponse, callback) {
+    callback(null, assistantResponse);
+  };
 };
-
-
-function trataResposta(err, resposta){
-
-    if(err){
-        console.log(err);
-        return;
-    }
-
-    if(resposta.output.text.length > 0){
-        console.log('Retorno -> '+ resposta.output.text);
-    }
-
-}
-
-// Server index page
-app.get("/", function (req, res) {
-    res.send("Deployed!");
-});
-
-// Facebook Webhook
-// Used for verification
-app.get("/webhook/facebook", function (req, res) {
-    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-      console.log("Verified webhook");
-      res.status(200).send(req.query["hub.challenge"]);
-    } else {
-      console.error("Verification failed. The tokens do not match.");
-      res.sendStatus(403);
-    }
-  });
